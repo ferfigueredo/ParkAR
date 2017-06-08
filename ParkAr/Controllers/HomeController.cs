a3983eaf0cfb437260f69bc602a6d73d3c43c163
Mordasini <fernando.a.mordasini@accenture.com>
2017-06-08 17:50:07 -0300

    modific navbar y homepage

--- ParkAr/Controllers/HomeController.cs
+++ ParkAr/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace ParkAr.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
+            ViewBag.Message = "Somos un grupo de jovenes emprendedores que creemos que estamos en la faz de la tierra para hacer grandes cosas. Estamos constantemente centrados en la innovación. Creemos en lo simple no en lo complejo. Creemos que necesitamos poseer y controlar las tecnologías primarias detrás de los servicios que hacemos, y participar solamente en los mercados donde podemos hacer una contribución significativa. Creemos en decir no a miles de proyectos, para que realmente podamos concentrarnos en los pocos que son verdaderamente importantes y significativos para nosotros. Creemos en la profunda colaboración y polinización cruzada de nuestros grupos, que nos permiten innovar de una manera que otros no pueden. Y francamente, no nos conformamos con nada menos que la excelencia en cada area de la empresa, y tenemos la honestidad de sí mismo para admitir que estamos equivocados y el coraje para cambiar. Y creo que independientemente de quién está haciendo que parte del trabajo los valores están tan incrustados en esta empresa que estamos convencidos de que nos ira muy bien.";
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Nuestros medios de contacto";
+            ViewBag.Title = "Contacto";
 
             return View();
         }
